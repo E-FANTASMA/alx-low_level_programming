f31195a9bb3bf8cac26dc3f20f58b49e1db48488
jeremiah <jeremiah@gmail.com>
2023-07-13 14:43:02 +0100

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 /**
-  *main - Entry point
-  *return : always 0 (success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
