c6d6bc6582d8e954beb7645b187ddb69777e33e2
jeremiah <jeremiah@gmail.com>
2023-07-18 14:49:58 -0700

    NEW

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * print_sign - finds the sign of n.
+ *
+ * @n: the number to be checked.
+ *
+ *  Return: 1 if (n>1), 0 if (n = 0), -1 if (n<0), otherwise /.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
