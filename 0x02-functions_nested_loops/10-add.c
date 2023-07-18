d4bf506d9854811c2455e706f26438c0fbdf0c3b
jeremiah <jeremiah@gmail.com>
2023-07-18 15:32:32 -0700

    new

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two digits.
+ *
+ * @i: first digit.
+ *
+ * @j: second digit.
+ *
+ * Return: the sum of the two digits.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
