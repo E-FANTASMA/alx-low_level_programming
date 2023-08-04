64b0818c7e1ff2de37b8e142624d913ad13e284f
jeremiah <jeremiah@gmail.com>
2023-08-04 10:29:21 -0700

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ *
+ * @arc: line arguments
+ *
+ * @arv: array to be used
+ *
+ * Return: success
+ */
+
+int main(int arc, char *arv[])/*prototype to be included in main.h */
+{
+	printf("%s\n", arv[arc * 0]);
+
+	return (0);
+}
