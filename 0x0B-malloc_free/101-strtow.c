a7165f8ffcf6b62e62c0241b6a9000853ac0afbd
jeremiah <jeremiah@gmail.com>
2023-08-08 13:27:02 -0700

    new

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,75 +1,109 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void main_one(char **, char *);
+void word(char **, char *, int, int, int);
 
 /**
- * strtow - concatenates arguments
+ * strtow - divide a given string into words.
  *
- *@str: string
- *
- *@av: arguments
+ * @str: string to be divided
  *
  * Return: success
  */
 
 char **strtow(char *str)
 {
-	int a;
-	int b;
-	int c;
-	int w;
-	int count;
-	int m;
-	int wordf;
-	char **p;
-	char *x;
+	int a, b, len;
+	char **wrd;
 
-	w = 0;
-	b = 0;
-	a = 0;
-	count = 0;
-	if (*str == '\0' || str == NULL)
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
-	for (a = 0; str[a] != '\0'; a++)
+
+	a = b = len = 0;
+	while (str[a])
 	{
-		if (str[a] == ' ' && (str[a + 1] != ' ' || str[a + 1] == '\0'))
-			w++;
-	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == NULL)
-		return (NULL);
-	for (wordf = 0; str[wordf] && b <= w; wordf++)
-	{
-		count = 0;
-		if (str[wordf] != ' ')
+		if (b == 0 && str[a] != ' ')
+			b = 1;
+		if (a > 0 && str[a] == ' ' && str[a - 1] != ' ')
 		{
-			for (a = wordf ; str[a] != '\0'; a++)
-			{
-				if (str[a] == ' ')
-					break;
-				count++;
-			}
-			*(p + b) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + b) == NULL)
-			{
-				for (c = 0; c <= b; c++)
-				{
-					x = p[c];
-					free(x);
-				}
-				free(p);
-				return (NULL);
-			}
-			for (m = 0; wordf < a; wordf++)
-			{
-				p[b][m] = str[wordf];
-				m++;
-			}
-			p[b][m] = '\0';
-			b++;
+			b = 0;
+			len++;
 		}
+		a++;
 	}
-	p[b] = NULL;
 
-	return (p);
+	len += b == 1 ? 1 : 0;
+	if (len == 0)
+		return (NULL);
+
+	wrd = (char **)malloc(sizeof(char *) * (len + 1));
+	if (wrd == NULL)
+		return (NULL);
+
+	main_one(wrd, str);
+	wrd[len] = NULL;
+	return (wrd);
+}
+
+/**
+ * main_one - fetches words to an array
+ *
+ * @wrd: words
+ *
+ * @str: string to be used
+ */
+
+void main_one(char **wrd, char *str)
+{
+	int a, b, c, d;
+
+	a = b = c = 0;
+	while (str[a])
+	{
+		if (d == 0 && str[a] != ' ')
+		{
+			c = a;
+			d = 1;
+		}
+
+		if (a > 0 && str[a] == ' ' && str[a - 1] != ' ')
+		{
+			word(wrd, str, c, a, b);
+			b++;
+			d = 0;
+		}
+
+		a++;
+	}
+
+	if (d == 1)
+		word(wrd, str, c, a, b);
+}
+
+/**
+ * word - creates a word and insert it into the array
+ *
+ * @wrd: the array of strings
+ *
+ * @str: string
+ *
+ * @start: starting
+ *
+ * @end: the stopping index of the word
+ *
+ * @index: index of array
+ */
+
+void word(char **wrd, char *str, int start, int end, int index)
+{
+	int a, b;
+
+	a = end - start;
+	wrd[index] = (char *)malloc(sizeof(char) * (a + 1));
+
+	for (b = 0; start < end; start++, b++)
+		wrd[index][b] = str[start];
+	wrd[index][b] = '\0';
 }
