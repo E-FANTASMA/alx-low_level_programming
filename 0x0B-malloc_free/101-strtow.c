cfe9441cee0210cd250a6bdc2d14446ebb5bc397
jeremiah <jeremiah@gmail.com>
2023-08-08 14:31:49 -0700

    new

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,109 +1,106 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-void main_one(char **, char *);
-void word(char **, char *, int, int, int);
+int wrd_len(char *s);
+int wrd_cnt(char *str);
+char **strtow(char *str);
 
 /**
- * strtow - divide a given string into words.
+ * wrd_len - locates the index marking
  *
- * @str: string to be divided
+ * @s:  string used
+ *
+ * Return: success
+ */
+
+int wrd_len(char *s)
+{
+	int i = 0, len = 0;/* len means length*/
+
+	while (*(s + i) && *(s + i) != ' ')
+	{
+		len++;
+		i++;
+	}
+
+	return (len);
+}
+
+/**
+ * wrd_cnt - counts words in a string
+ *
+ * @s: string
+ *
+ * Return: success
+ */
+
+int wrd_cnt(char *s)
+{
+	int i = 0, wrd = 0, len = 0;
+
+	for (i = 0; *(s + i); i++)
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		if (*(s + i) != ' ')
+		{
+			wrd++;
+			i += wrd_len(s + i);
+		}
+	}
+
+	return (wrd);
+}
+
+/**
+ * strtow - Splits a string into words
+ *
+ * @str: string to be split.
  *
  * Return: success
  */
 
 char **strtow(char *str)
 {
-	int a, b, len;
-	char **wrd;
+	char **st;
+	int i = 0, wrd, w, letters, l;
 
-	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
-	a = b = len = 0;
-	while (str[a])
+	wrd = wrd_cnt(str);
+	if (wrd == 0)
+		return (NULL);
+
+	st = malloc(sizeof(char *) * (wrd + 1));
+	if (st == NULL)
+		return (NULL);
+
+	for (w = 0; w < wrd; w++)
 	{
-		if (b == 0 && str[a] != ' ')
-			b = 1;
-		if (a > 0 && str[a] == ' ' && str[a - 1] != ' ')
+		while (str[i] == ' ')
+			i++;
+
+		letters = wrd_len(str + i);
+
+		st[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (st[w] == NULL)
 		{
-			b = 0;
-			len++;
+			for (; w >= 0; w--)
+				free(st[w]);
+
+			free(st);
+			return (NULL);
 		}
-		a++;
+
+		for (l = 0; l < letters; l++)
+			st[w][l] = str[i++];
+
+		st[w][l] = '\0';
 	}
+	st[w] = NULL;
 
-	len += b == 1 ? 1 : 0;
-	if (len == 0)
-		return (NULL);
-
-	wrd = (char **)malloc(sizeof(char *) * (len + 1));
-	if (wrd == NULL)
-		return (NULL);
-
-	main_one(wrd, str);
-	wrd[len] = NULL;
-	return (wrd);
-}
-
-/**
- * main_one - fetches words to an array
- *
- * @wrd: words
- *
- * @str: string to be used
- */
-
-void main_one(char **wrd, char *str)
-{
-	int a, b, c, d;
-
-	a = b = c = 0;
-	while (str[a])
-	{
-		if (d == 0 && str[a] != ' ')
-		{
-			c = a;
-			d = 1;
-		}
-
-		if (a > 0 && str[a] == ' ' && str[a - 1] != ' ')
-		{
-			word(wrd, str, c, a, b);
-			b++;
-			d = 0;
-		}
-
-		a++;
-	}
-
-	if (d == 1)
-		word(wrd, str, c, a, b);
-}
-
-/**
- * word - creates a word and insert it into the array
- *
- * @wrd: the array of strings
- *
- * @str: string
- *
- * @start: starting
- *
- * @end: the stopping index of the word
- *
- * @index: index of array
- */
-
-void word(char **wrd, char *str, int start, int end, int index)
-{
-	int a, b;
-
-	a = end - start;
-	wrd[index] = (char *)malloc(sizeof(char) * (a + 1));
-
-	for (b = 0; start < end; start++, b++)
-		wrd[index][b] = str[start];
-	wrd[index][b] = '\0';
+	return (st);
 }
