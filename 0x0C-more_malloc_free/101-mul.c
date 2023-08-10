52dc1bface2ca8191d30109d4254c1bb923d0240
jeremiah <jeremiah@gmail.com>
2023-08-10 15:27:37 -0700

    done

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  multiplies two positive numbers
+ *
+ * @argc: number of arguments
+ *
+ * @argv: argument vector
+ *
+ * Return: success
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	if (argc != 3)/* number of arguments must be 3*/
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* atoi converts a string of characters to an integer value*/
+	num1 = atoi(argv[1]);
+	/* should be positive*/
+	if (num1 < 0)
+	{
+		return (0);
+	}
+
+	num2 = atoi(argv[2]);/* should be positive */
+
+	if (num2 < 0)
+	{
+		return (0);
+	}
+
+	result = num1 * num2;
+	printf("%d\n",result);
+
+	return (0);
+}
