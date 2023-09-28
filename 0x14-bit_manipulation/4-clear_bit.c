8941ee8b461a7ce84d1dd330db0090f42a4cf271
jeremiah <jeremiah@gmail.com>
2023-09-28 14:08:55 -0700

    10:09

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value
+ *
+ * @n: pointer
+ *
+ * @index: index
+ *
+ * Return: success
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 50)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+
+	return (1);
+}
